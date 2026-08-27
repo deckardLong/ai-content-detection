0253f906fedb43b558f7116b0dcecf6b900f262d
Deckard Long <gianglong311005@gmail.com>
2026-08-27 11:22:30 +0700

    Allow credentials

--- backend/app/main.py
+++ backend/app/main.py
@@ -45,7 +45,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.cors_origins,
     allow_methods=['*'],
-    allow_headers=['*']
+    allow_headers=['*'],
+    allow_credentials=True
 )
 
 app.include_router(health.router)
